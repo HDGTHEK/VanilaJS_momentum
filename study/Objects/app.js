2271a43396dd29f6fbc53e3f88126488b4e47842
Donggyun Ha <irvcaptain@knu.ac.kr>
2022-01-29 18:19:04 +0900

    untilObjects

--- study/Objects/app.js
+++ study/Objects/app.js
@@ -1,14 +1,21 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const playerName = "hdg";
+const playerPoints = 121223;
+const playerHandsome = false;
+const playerFat = "little bit"; //변수설정 -> not good
 
-// Get Item from Array
-console.log(daysOfWeek);
+const playerArray = ["hdg", 121223, false, "little bit"]; // 배열 -> not good
 
-// Add one more day to the array
-daysOfWeek.push("sun");
+//object!
+const player = {
+    name: "hdg",
+    points: 10,
+    fat: true,
+};
 
-console.log(daysOfWeek);
+console.log(player);
 
-const toBuy = ["potato", "tomato", "pizza"];
-toBuy.push("Kimbob");
+player.fat = false; //const는 못바꾸잖아? -> const는 object, 즉 player이고 그 안의 name,fat 등등 업데이트는 문제 X
+player.lastName = "wow";
+player.points = player.points + 30;
 
-console.log(toBuy[989544646]); //undefined
+console.log(player);
