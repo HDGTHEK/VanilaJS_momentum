451abb56d93e58c743c53ac6ab709d9976be182c
Donggyun Ha <irvcaptain@knu.ac.kr>
2022-01-29 22:55:14 +0900

    RealIntroducingJSFinished

--- introduciongJS/6_Conditonals/app.js
+++ introduciongJS/6_Conditonals/app.js
@@ -2,7 +2,7 @@ const age = parseInt(prompt("How old are you?"));
 
 
 if (isNaN(age) || age < 0) {
-    console.log("Are you insane? Write the fucking your age");
+    console.log("Are you insane? Write your fucking age");
    // condition === true
 }
 else if(age < 18) {
@@ -10,10 +10,13 @@ else if(age < 18) {
 }
 else if (age >= 18 && age < 50) {
     console.log("You can drink");
-    } // && == and, || == or
+    } // && -> and, || -> or
 else if (age > 50 && age <= 80) {
     console.log("You should be careful")    
 }
+else if (age === 100) {
+    console.log("God damn...")
+} // === -> 조건문 same인지 확인 !== -> 조건문 not same인지 확인
 else if (age > 80) {
     console.log("You will be fucked up!");
-}
+} // JS는 위에서 아래로 읽음 -> 위에거보다 먼저오면 ㅈ댐
