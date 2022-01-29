8adcaadc3baa588aafd3287878b9d58bc40774eb
Donggyun Ha <irvcaptain@knu.ac.kr>
2022-01-29 21:14:06 +0900

    introducingJavascript

---
+++ preStudy/5_Returns/app.js
@@ -0,0 +1,35 @@
+/*
+const age = 22;
+
+function calculateKrAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
+*/
+
+const calculator = {
+    plus: function (a, b) {
+        return a + b;
+    },
+    minus: function (a, b) {
+        return a - b;
+    },
+    times: function (a, b) {
+        return a * b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    power: function (a, b) {
+        return a ** b;
+    },
+};
+
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
