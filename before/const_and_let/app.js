ff86cf8eaccfd1a609ff6f9966c671da37e8fed7
Donggyun Ha <irvcaptain@knu.ac.kr>
2022-01-29 17:56:43 +0900

    firstcommit

---
+++ before/const_and_let/app.js
@@ -0,0 +1,12 @@
+const a = 5; //변화 X
+const b = 2;
+let myName = 'hdg'; // 변화 O
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Hadonggyun";
+
+console.log("your new name is " + myName);
---
+++ readme.txt
@@ -0,0 +1,3 @@
+//file: readme.txt
+ReactJS 공부 전에 
+노마드코더 VanilaJS 클론코딩으로 학습
