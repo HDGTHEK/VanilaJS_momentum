e424bec0e2066ac6a7540fb8e52799c3a9077953
Donggyun Ha <irvcaptain@knu.ac.kr>
2022-02-05 12:05:15 +0900

    clock

--- MomentumAPP/js/clock.js
+++ MomentumAPP/js/clock.js
@@ -1,5 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0"); //padStart & padEnd -> 가져야하는 길이 설정
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
+getClock();
+setInterval(getClock, 1000);
